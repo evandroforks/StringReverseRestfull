820c55081e16fa4db68a3768c69bea9dfcbc9d22
evandrocoan <evandrocoan@hotmail.com>
2018-05-08 15:24:47 -0300

    Reverted back to using @WebServlet tag on HelloWorld.java and
    HelloWorldServlet/WebContent/WEB-INF/web.xml

--- HelloWorldServlet/src/com/srccodes/example/HelloWorld.java
+++ HelloWorldServlet/src/com/srccodes/example/HelloWorld.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloWorld
  */
+@WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
