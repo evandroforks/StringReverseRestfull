418901da471526c4f193b65426874a201c1b20fe
evandrocoan <evandrocoan@hotmail.com>
2018-05-08 15:14:54 -0300

    Manually set the HelloWorld servlet home page on web.xml
    HelloWorldServlet/WebContent/WEB-INF/web.xml

--- HelloWorldServlet/src/com/srccodes/example/HelloWorld.java
+++ HelloWorldServlet/src/com/srccodes/example/HelloWorld.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloWorld
  */
-@WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
